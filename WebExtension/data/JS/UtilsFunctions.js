917f9b1cacc150fd94980a99bdc23910bca0a444
Mmoi <none>
2017-09-30 16:09:07 +0200

    Fix: unescape firefox compatibility

--- WebExtension/data/JS/UtilsFunctions.js
+++ WebExtension/data/JS/UtilsFunctions.js
@@ -154,6 +154,7 @@ function getTextFromHTML(HTMLString)
     HTMLString = HTMLString.replace(/<(\/?[ubi])>/g, "&lt;$1&gt;"); // match : <u><b><i>
     HTMLString = HTMLString.replace(/<font color="(#[0-9a-fA-F]{6})">/g, "&lt;font color=\"$1\"&gt;"); // match : <font color="#XXXXXX">
     HTMLString = HTMLString.replace(/<\/font>/g, "&lt;/font&gt;"); // match : </font>
+    HTMLString = HTMLString.replace(/<br>/g, "\n"); // Saut de ligne
 
     // On dé-echape le chaîne
     var element = document.createElement('div');
