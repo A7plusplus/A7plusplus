a2898072794b834044d5e4ec8117b9d5ced85d06
Mmoi <mmoi.addic7ed@gmail.com>
2018-06-29 08:55:12 +0200

    Hotfix: edition text removed in some cases

--- WebExtension/data/JS/Settings.js
+++ WebExtension/data/JS/Settings.js
@@ -7,8 +7,8 @@
 // Déclaration de l'objet contenant tous les réglages
 var A7Settings = {
 
-    MAJOR_VERSION_INFO   : 'A7++ 2.3',
-    MINOR_VERSION_INFO   : '2.3 stable',
+    MAJOR_VERSION_INFO   : 'A7++ 2.3.1',
+    MINOR_VERSION_INFO   : '2.3.1 stable',
 
     // Longueur en caractères
     maxPerLineOneLineSETTING : 37,
